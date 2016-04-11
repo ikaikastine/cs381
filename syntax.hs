8cb621b9cad8f96596e4a0403e557fce3b532335
ikaikastine <ikaikastine@gmail.com>
2016-04-11 14:54:00 -0700

    fixed name for value constructor

--- syntax.hs
+++ syntax.hs
@@ -2,22 +2,22 @@
 --(a) Define the abstract syntax for Mini Logo as a Haskell data type
 
 type Name = String
-type Num = Int
+type Numb = Int
 
-data Pos = PO Num | PS Name deriving Show
---data PenPos = (Pos, Pos)
+data Pos = PO Numb | PS Name deriving Show
+data PenPos = PP (Pos, Pos)
 
---data Pars = Parse [Name] deriving Show
-type Pars = [String]
---data Vals = Values [Name] deriving Show
-type Vals = [Int]
+data Pars = Parse [Name] deriving Show
+--type Pars = [String]
+data Vals = Values [Name] deriving Show
+--type Vals = [Int]
 data Mode = Up | Down deriving Show
 
 data Cmd = Pen Mode
-          | MoveTo (Pos, Pos)
+          | MoveTo PenPos
           | Def Name Pars Cmds
           | Call Name Vals
-          deriving Show
+          --deriving Show
 type Cmds = [Cmd]
 
 --(b) Write a Mini Logo macro vector that draws a line from a given position
@@ -25,7 +25,7 @@ type Cmds = [Cmd]
 --syntax, that is, as a Haskell data type value.
 
 --Abstract Syntax:
-vector = Def "vector" [x1, y1, x2, y2] [Pen Up, MoveTo (x1, y1), Pen Down, MoveTo (x2, y2)]
+vector = Def "vector" (Parse ["x1", "y1", "x2", "y2"]) [Pen Up, MoveTo (Parse "x1", Parse "y1"), Pen Down, MoveTo (Parse "x2", Parse "y2")]
 callVec = Call "vector" [1,1,2,2]
 
 -- Exercise 2. Digital Circuit Design language
