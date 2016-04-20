569b106b5907d82974786898906c00287b500e2e
Kevin Stine <ikaikastine@gmail.com>
2016-04-20 11:57:06 -0700

    updated section 2

--- syntax.hs
+++ syntax.hs
@@ -34,12 +34,47 @@ steps :: Int -> Cmd
 steps a | a <= 1 = Func [Call "vector" [0, 0, 0, 1], Call "vector" [0, 1, 1, 1]]
 steps a = Func [steps (a-1), Func [Call "vector" [a-1, a-1, a-1, a], Call "vector" [a-1, a, a, a]]]
 
---steps a = Func [steps (a-1), Func [Call "vector" [a-1, a-1, a-1, a], Call "vector" [a-1, a, a, a]]
 -- Exercise 2. Digital Circuit Design language
+--(a) Define the abstract syntax for the above language as a Haskell data type
+data Circuit = Circuit Gates Links
 
+data Gates = Gate Int GateFn Gates | GNone
 
+data GateFn = And | Or | Xor | Not deriving Show
 
+data Pair = Pair Int Int
 
+data Links = Link Pair Pair Links | LNone
+
+--(b) Represent the half adder circuit in abstract syntax, that is, as a Haskell
+--data type
+halfadder = Circuit (Gate 1 Xor
+                    (Gate 2 And
+                    GNone))
+
+                    (Link (Pair 1 1) (Pair 2 1)
+                    (Link (Pair 1 2) (Pair 2 2)
+                    LNone))
+
+--(c) Define a Haskell function that implements a pretty printer for the
+--abstract syntax
+printPair :: Pair -> String
+printPair (Pair x y) = show x ++ "." ++ show y
+
+printGateFn :: GateFn -> String
+printGateFn And = "and"
+printGateFn Or = "or"
+printGateFn Xor = "xor"
+printGateFn Not = "not"
+
+printGates :: Gates -> String
+printGates GNone = ""
+printGates (Gate i gatefn gates) = show i ++ ":" ++ show gatefn ++ ";\n"
+            ++ printGates gates
+
+printLinks :: Links -> String
+printLinks LNone = ""
+printLinks (Link pair1 pair2 links)
 
 
 
