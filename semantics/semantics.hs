47bf279c3ca318a9642ad3f19a2d67bb666c62f3
Kevin Stine <ikaikastine@gmail.com>
2016-05-02 11:24:01 -0700

    updated exercise 2

--- semantics/semantics.hs
+++ semantics/semantics.hs
@@ -1,4 +1,4 @@
--- Kevin Stine
+ -- Kevin Stine
 
 -- Exercise 1. A Stack Language
 
@@ -37,15 +37,19 @@ eval p = sem p (Just [])
 
 -- Test data
 test1 = [LD 3, DUP, ADD, DUP, MULT] -- [3] -> [3,3] -> [6] -> [6,6] -> Just [36]
-
+test2 = [LD 3, ADD] -- Nothing
+test3 = [] -- Just []
 
 
 -- Exercise 2. Extending the Stack Language by Macros
 -- (a) Extend the abstract syntax to represent macro definitions and calls,
 -- that is, give a correspondingly changed data definition for Cmd.
+data Cmd2 = C Cmd
+          | DEF String Prog
+          | CALL String
+          deriving Show
 
-
-
+type Prog2 = [Cmd2]
 
 -- (b) Define a new type State to represent the sate for the new language. The
 -- state includes the macro definitions and the stack. Please note that a macro
@@ -55,10 +59,34 @@ test1 = [LD 3, DUP, ADD, DUP, MULT] -- [3] -> [3,3] -> [6] -> [6,6] -> Just [36]
 -- could thus be defined as follows
 
 type Macros = [(String, Prog)]
-
+type State = (Macros, Maybe Stack)
 
 -- (c) Define the semantics for the extended language as a function sem2. As in
 -- exercise 1, you probably want to define an auxilary function semCmd2 for the
 -- semantics of individual operations.
+sem2 :: Prog2 -> E
+sem2 [] a = a
+sem2 (x:xs) a = sem2 xs (semCmd2 x a)
+
+semCmd2 :: Cmd2 -> E
+semCmd2 (C c) (m, st) = m, semCmd (C c) st
+semCmd2 (DEF cmd p) (m, st) = [(cmd, p)] ++ m, st
+semCmd2 (CALL cmd) (m, st) = semCmd2 cmd (m, st)
+semCmd2 c          (m, st) = (m, map semCmd2 nCmd)
+                            where nCmd = snd (c, prog):m 
 
 -- Exercise 3. Mini Logo
+data Cmd = Pen Mode
+         | MoveTo Int Int
+         | Seq Cmd Cmd
+
+data Mode = Up | Down
+
+type State = (Mode, Int, Int)
+
+type Line = (Int, Int, Int, Int)
+type Lines = [Line]
+
+semS :: Cmd -> State -> (State, Lines)
+
+sem' :: Cmd -> Lines
