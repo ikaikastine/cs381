50bb791114ac1e8de5ed955a761d3f582818f36c
ikaikastine <ikaikastine@gmail.com>
2016-05-01 22:09:18 -0700

    finished first section. add more tests

--- semantics/semantics.hs
+++ semantics/semantics.hs
@@ -13,12 +13,31 @@ data Cmd = LD Int
           | ADD
           | MULT
           | DUP
+          deriving Show
 
 type Stack = [Int]
 
+type D = Maybe Stack -> Maybe Stack
+
 sem :: Prog -> D
+sem [] a = a
+sem (x:xs) a = sem xs (semCmd x a)
 
 semCmd :: Cmd -> D
+semCmd (LD a) xs = case xs of Just xs         -> Just ([a] ++ xs)
+                              _               -> Nothing
+semCmd (ADD)  xs = case xs of Just (x1:x2:xs) -> Just ([x1+x2] ++ xs)
+                              _               -> Nothing
+semCmd (MULT) xs = case xs of Just (x1:x2:xs) -> Just ([x1*x2] ++ xs)
+                              _               -> Nothing
+semCmd (DUP)  xs = case xs of Just (x1:xs)    -> Just ([x1,x1] ++ xs)
+                              _               -> Nothing
+eval :: Prog -> Maybe Stack
+eval p = sem p (Just [])
+
+-- Test data
+test1 = [LD 3, DUP, ADD, DUP, MULT] -- [3] -> [3,3] -> [6] -> [6,6] -> Just [36]
+
 
 
 -- Exercise 2. Extending the Stack Language by Macros
