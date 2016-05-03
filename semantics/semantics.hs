440f125af0bd3742fb455dbad13c085e6e9cdf9a
Kevin Stine <ikaikastine@gmail.com>
2016-05-03 10:23:41 -0700

    updates sematics

--- semantics/semantics.hs
+++ semantics/semantics.hs
@@ -13,12 +13,17 @@ data Cmd = LD Int
           | ADD
           | MULT
           | DUP
-          deriving Show
+          deriving (Eq, Show)
 
 type Stack = [Int]
 
 type D = Maybe Stack -> Maybe Stack
 
+{-prog :: [Cmd] -> Stack -> Maybe Stack
+prog ([x]) y = semCmd x y
+prog (x:xs) ys = prog xs (semCmd x ys)
+prog [] _ = Nothing
+-}
 sem :: Prog -> D
 sem [] a = a
 sem (x:xs) a = sem xs (semCmd x a)
@@ -44,12 +49,12 @@ test3 = [] -- Just []
 -- Exercise 2. Extending the Stack Language by Macros
 -- (a) Extend the abstract syntax to represent macro definitions and calls,
 -- that is, give a correspondingly changed data definition for Cmd.
-data Cmd2 = C Cmd
-          | DEF String Prog
-          | CALL String
-          deriving Show
-
+type Name = String
 type Prog2 = [Cmd2]
+data Cmd2 = Define Name Prog2
+          | Call Name
+          | Basic Cmd
+          deriving (Eq, Show)
 
 -- (b) Define a new type State to represent the sate for the new language. The
 -- state includes the macro definitions and the stack. Please note that a macro
@@ -58,35 +63,31 @@ type Prog2 = [Cmd2]
 -- definitions can be stored in a list. A type to represent macro definitions
 -- could thus be defined as follows
 
-type Macros = [(String, Prog)]
+type Macros = [(Name, Prog2)]
 type State = (Macros, Maybe Stack)
-
-data ProgMacro = P Prog2
-             | M Macros
-             deriving Show
-
-type E = Maybe State -> Maybe State
 
 -- (c) Define the semantics for the extended language as a function sem2. As in
 -- exercise 1, you probably want to define an auxilary function semCmd2 for the
 -- semantics of individual operations.
-sem2 :: Prog2 -> E
-sem2 [] a = a
-sem2 (x:xs) a = sem2 xs (semCmd2 x a)
+sem2 :: Prog2 -> State -> Maybe State
+sem2 [] _ = Nothing
+sem2 ([x]) y = semCmd2 x y
+sem2 (p:ps) s = sem2 ps (Maybe (semCmd2 p s))
 
-semCmd2 :: Cmd2 -> E
-semCmd2 (C c) (m, st) = m, semCmd (C c) st
-semCmd2 (DEF cmd p) (m, st) = [(cmd, p)] ++ m, st
-semCmd2 (CALL cmd) (m, st) = semCmd2 cmd (m, st)
-semCmd2 c          (m, st) = (m, map semCmd2 nCmd)
-                            where nCmd = snd (c, prog):m
+semCmd2 :: Cmd2 -> State -> Maybe State
+semCmd2 (Basic c) (m, s) = Just (m, semCmd c s)
+semCmd2 (Define n p) (m,s) = Just (m ++ [(n,p)], s)
+semCmd2 (Call n) (m,s) = sem2 (lookup n m) (m,s)
 
-eval2 :: ProgMacro -> Macros
-eval2 p = sem2 p (Just [])
 
-macroTest1 = [DEF "foo" [DUP, ADD, MULT]] -- [("foo", [DUP, ADD, MULT])]
+
+--eval2 :: ProgMacro -> Macros
+--eval2 p = sem2 p (Just [])
+
+--macroTest1 = [Define "foo" [DUP, ADD, MULT]] -- [("foo", [DUP, ADD, MULT])]
 
 -- Exercise 3. Mini Logo
+{-
 data Cmd = Pen Mode
          | MoveTo Int Int
          | Seq Cmd Cmd
@@ -101,3 +102,4 @@ type Lines = [Line]
 semS :: Cmd -> State -> (State, Lines)
 
 sem' :: Cmd -> Lines
+-}
