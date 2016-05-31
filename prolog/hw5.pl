375907de57a845b2740d09d6778e0c8ffdf11dd6
ikaikastine <ikaikastine@gmail.com>
2016-05-31 12:50:05 -0700

    finished exercise 1

--- prolog/hw5.pl
+++ prolog/hw5.pl
@@ -25,6 +25,10 @@ schedule(S, P, T) :- enroll(S, C), when(C, T), where(C, P).
 /* (b) Usage/2 - P: Place, T: Time */
 usage(P, T) :- when(C, T), where(C, P).
 
+/* (c) Conflict/2 - C1: Course 1, C2: Course 2: */
+conflict(C1,C2) :- where(C1,X), where(C2,X), when(C1,Y), when(C2,Y), C1\=C2.
 
+/* (d) Meet/2 - S: Student 1, R: Student 2 */
+meet(S,R) :- schedule(S,P,T), schedule(R,P,T), schedule(S,P,T1), schedule(R,P,T2), T1\==T2+1.
 
 /* Exercise 2 */
